278320d1c88c383706a0e16318b7b88886e49840
Luka17 <lukajesekac@gmail.com>
2018-09-30 10:45:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSGO to CS 1.6
+
+This is the replacement for mp.dll that makes the movement, guns and other things like in CSGO.
---
+++ version/appversion.h
@@ -0,0 +1,19 @@
+#ifndef __APPVERSION_H__
+#define __APPVERSION_H__
+
+// 
+// This file is generated automatically.
+// Don't edit it.
+// 
+
+// Version defines
+#define APP_VERSION "5.7.0.0-dev"
+
+#define APP_COMMIT_DATE "Sep 06 2018"
+#define APP_COMMIT_TIME "20:14:20"
+
+#define APP_COMMIT_SHA ""
+#define APP_COMMIT_URL "/commits/"
+
+#endif //__APPVERSION_H__
+
